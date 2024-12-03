c99624bb0e367eca9e10b58e5ee9e31b6c485812
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-12-03 15:37:18 +0530

    Divisible

---
+++ Div_3or7.py
@@ -0,0 +1,7 @@
+a=int(input('Enter a number'))
+if(a%3==0):
+    print(a,'is divisible by 3')
+elif(a%7==0):
+    print(a,'is divisible by 7')
+else:
+    print('The number is not divisible by 3 or 7')
